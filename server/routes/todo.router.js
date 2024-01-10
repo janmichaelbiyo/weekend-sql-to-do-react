de04769a1b2a370cdb7079b8bf95325b5b7e7c43
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-01-09 21:48:11 -0600

    got the router server to work with the get file

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,6 +4,22 @@ const pool = require('../modules/pool.js');
 
 // GET
 
+router.get('/', (req, res) => {
+     const todoQuery = 'SELECT * FROM "todo";';
+
+     pool
+        .query(todoQuery)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('this is it stop', error);
+
+            res.sendStatus(500);
+        })
+});
+
+
 // POST
 
 // PUT
