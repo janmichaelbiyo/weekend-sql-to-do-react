68696f0ebaffad4e95027482dd15167ce1fe1d3c
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-01-15 23:00:37 -0600

    did the database work on ths issue will try an do more on the next steps

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -43,6 +43,40 @@ router.post('/', (req, res) => {
 
 // PUT
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const todoData = req.body;
+    const queryTodoUpdate = `UPDATE "todo" SET "is_complete" = TRUE;`;
+
+    pool
+        .query(queryTodoUpdate, [todoData.is_complete, id])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('whats going on', error);
+
+            res.sendStatus(500);
+        })
+
+})
+
+
 // DELETE
+
+router.delete('/:id', (req,res) => {
+    const queryDeleteTodo = `DELETE FROM "todo" WHERE "id" = ${req.params.id}`;
+
+    pool    
+        .query(queryDeleteTodo)
+        .then((result) => {
+            req.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('fix me now dude', error);
+
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
